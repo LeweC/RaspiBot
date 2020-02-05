f3a0ad2077aab9d1fcea87fe60d2420cec1c007d
LeweC <lewe22lewe@googlemail.com>
2020-02-05 15:12:33 +0100

    adding turn commands :point_left:

--- Client/main.py
+++ Client/main.py
@@ -6,11 +6,26 @@ if True:
     move.init_all()
     try:
         while 1:
-            move.move(step, 35, 'no')
-            step += 1
-            if step > 4:
-                step = 1
-            time.sleep(0.5)
-    
+            for x in range(24):
+                move.move(step, 35, 'no')
+                step += 1
+                if step > 4:
+                    step = 1
+                time.sleep(0.2)
+
+            for i in range(18):
+                move.move(step, 35, 'left')
+                time.sleep(0.2)
+                step += 1
+                if step == 5:
+                    step = 1
+            """ 
+            for i in range(18):
+                move.move(step, 35, 'left')
+                time.sleep(0.2)
+                step += 1
+                if step == 5:
+                    step = 1
+            """
     except:
         move.clean_all()
