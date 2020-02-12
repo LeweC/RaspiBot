c139ae66a7c99e7b674cf9d6c6ef6fb35dfe55af
LeweC <lewe22lewe@googlemail.com>
2020-02-12 16:13:04 +0100

    Sensor testing :sound:

--- Client/main.py
+++ Client/main.py
@@ -5,20 +5,20 @@ import time
 if True:
     step = 1
     move.init_all()
-    try:
-        while(ultrasonic.measure() >= 15):
-            print("fowart")        
-            move.move(step, 35, 'no')
-            time.sleep(0.2)
-            step += 1
-            if step == 5:
-               step = 1
-        else:
-            print("turn")      
-            move.move(step, 35, 'left')
-            step += 1
-            if step > 4:
-                step = 1
-            time.sleep(0.2)    
-    except:
-        move.clean_all()
+    
+    while(ultrasonic.measure() >= 15):
+        print("fowart")        
+        move.move(step, 35, 'no')
+        time.sleep(0.2)
+        step += 1
+        if step == 5:
+           step = 1
+    else:
+        print("turn")      
+        move.move(step, 35, 'left')
+        step += 1
+        if step > 4:
+            step = 1
+        time.sleep(0.2)    
+    
+    
