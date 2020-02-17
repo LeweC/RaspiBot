2fb80fec96859791340f8cefc86909d44a9da923
LeweC <lewe22lewe@googlemail.com>
2020-02-17 15:43:32 +0100

    Sensor testing :sound:

--- Client/main.py
+++ Client/main.py
@@ -5,20 +5,27 @@ import time
 if True:
     step = 1
     move.init_all()
-    while 1:
-        while ultrasonic.measure() >= 15:
-            print("forward")
-            print("------")        
-            move.move(step, 35, 'no')
-            time.sleep(0.2)
-            step += 1
-            if step > 4:
-                step = 1
-        else:
-            print("turn")
-            print("------")
-            move.move(step, 35, 'left')
-            step += 1
-            if step == 5:
-                step = 1
-            time.sleep(0.2)
+    try:
+        while 1:
+            while ultrasonic.measure() >= 15:
+                print("forward")
+                print(step)
+                print("------")        
+                move.move(step, 35, 'no')
+                time.sleep(0.2)
+                step += 1
+                if step > 4:
+                  step = 1
+            else:
+                print("turn")
+                print(step)
+                print("------")
+                move.move(step, 35, 'left')
+                step += 1
+                if step == 5:
+                    step = 1
+                time.sleep(0.2)
+    except KeyboardInterrupt:
+        print("Ending Scipt")
+        ultrasonic.closing()
+        time.sleep(1)
