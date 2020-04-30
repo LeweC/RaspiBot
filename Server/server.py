4fadbfc9fb078274a4d0b5d6ffa89aece2b18528
LeweC <lewe22lewe@googlemail.com>
2020-04-30 22:00:10 +0200

    Website testing :collision:

---
+++ Server/server.py
@@ -0,0 +1,9 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("192.168.178.112", 8001))
+s.listen(5)
+
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established.")
