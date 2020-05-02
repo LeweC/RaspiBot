982434dd1929651069f261e2a4bf8b63013dd214
LeweC <lewe22lewe@googlemail.com>
2020-05-02 22:38:12 +0200

    Website testing :collision:

--- Server/server.py
+++ Server/server.py
@@ -2,23 +2,20 @@ import asyncio
 import websockets
 
 async def hello(websocket, path):
-    try:
-        async for message in websocket:
-            print(f"< {message}")
+    async for message in websocket:
+        print(f"< {message}")
 
-            if message == "Standing":
-                greeting = f"{message}!"
-            else:
-                greeting = f"Moving {message}!"
-                await websocket.send(greeting)
-                print(f"> {greeting}")
-    except:
-        print('Reconnecting')
-        await websockets.connect("ws://192.168.178.112:8001")
+        if message == "Standing":
+            greeting = f"{message}!"
+        else:
+            greeting = f"Moving {message}!"
+            await websocket.send(greeting)
+            print(f"> {greeting}")
+    
         
     
 
-start_server = websockets.serve(hello, "192.168.178.112", 8001)
+start_server = websockets.serve(hello, "192.168.178.112", 8001, ping_interval=20)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
