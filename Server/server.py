05a578594184084bfd85587cc3af9ad640ed3960
LeweC <lewe22lewe@googlemail.com>
2020-05-02 22:29:46 +0200

    Website testing :collision:

--- Server/server.py
+++ Server/server.py
@@ -14,7 +14,7 @@ async def hello(websocket, path):
                 print(f"> {greeting}")
     except:
         print('Reconnecting')
-        await websockets.connect("192.168.178.112:8765")
+        await websockets.connect("ws://192.168.178.112:8001")
         
     
 
