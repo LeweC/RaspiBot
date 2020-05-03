6be8fc0b7910c77735c3c6e435e12bef3d62af16
LeweC <lewe22lewe@googlemail.com>
2020-05-03 19:02:58 +0200

    Website testing :collision:

--- Server/server.py
+++ Server/server.py
@@ -23,7 +23,7 @@ async def hello(websocket, path):
     
 start_server = websockets.serve(hello, "192.168.178.112", 8001, ping_interval=None)
 
-asyncio.get_event_loop().run_until_complete(start_server)
+#asyncio.get_event_loop().run_until_complete(start_server)
 #asyncio.get_event_loop().run_forever()
 
 if True:
