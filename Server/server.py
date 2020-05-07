479a1bf5ef0e71a12e21ca5eac34c8522798945e
LeweC <lewe22lewe@googlemail.com>
2020-05-08 01:14:42 +0200

    Website testing :collision:

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,4 @@
-import asyncio
+'''import asyncio
 import websockets
 import main
 direction = ""
@@ -29,4 +29,15 @@ asyncio.get_event_loop().run_forever()
 while True:
     if direction != "":
         print(direction)
-    main.moving(direction)
+    main.moving(direction)'''
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("192.168.178.112", 8001))
+s.listen(5)
+
+while True:
+    # now our endpoint knows about the OTHER endpoint.
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established.")
+    clientsocket.send(bytes("Hey there!!!","utf-8"))
