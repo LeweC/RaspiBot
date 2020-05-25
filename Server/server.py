fd1d64162c790a826fa9eaa77d7f1598ef3628f7
LeweC <lewe22lewe@googlemail.com>
2020-05-25 13:56:32 +0200

    While loop test :collision:

--- Server/server.py
+++ Server/server.py
@@ -6,20 +6,25 @@ async def hello(websocket, path):
     async for message in websocket:
         try:
             print(f"< {message}")
+            global direction 
             if message == "Standing":
                 greeting = f"{message}!"
                 print(f"> {greeting}")
-                main.moving(message)
+                direction = message
                 await websocket.send(greeting)
             else:
                 greeting = f"Moving {message}!"
                 print(f"> {greeting}")
-                main.moving(message)
+                direction = message
                 await websocket.send(greeting)
         except KeyboardInterrupt:
             print("Ending Scipt")
     
 start_server = websockets.serve(hello, "192.168.178.36", 8001, ping_interval=None)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+while True:
+    main.moving(direction)
+
+
+#asyncio.get_event_loop().run_until_complete(start_server)
+#asyncio.get_event_loop().run_forever()
