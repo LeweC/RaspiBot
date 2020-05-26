89b3479daa04e6371c175a91bd344d429c78ff89
LeweC <lewe22lewe@googlemail.com>
2020-05-27 00:02:02 +0200

    Internet accessibility :globe_with_meridians:

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -32,7 +32,7 @@ namespace RaspiBot
 
         private void webSocket(string test)
         {
-            using (var ws = new WebSocket("ws://31.16.67.247:8001"))
+            using (var ws = new WebSocket("ws://31.16.67.247:443"))
             {
                 ws.OnOpen += (sender, e) => {
                     lbl_connection.Text = "Connected to RaspiBot";
