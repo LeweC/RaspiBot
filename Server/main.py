9c9ea8bd51bb3802c76968bdb47191f433ad7b0a
LeweC <lewe22lewe@googlemail.com>
2020-08-11 20:17:45 +0200

    expandability and cleaning

--- Server/main.py
+++ Server/main.py
@@ -4,20 +4,11 @@ import time
 import random
 
 def handler(instructions):
-    if instructions[0] == "Forward":
-        move.forward()
-    elif instructions[0] == "Left":
-        move.left()    
-    elif instructions[0] == "Right":
-        move.right()
-    elif instructions[0] == "Standing":
-        move.stand()
-    elif instructions[0] == "Auto":
-        move.autonom()
+    directions(instructions)
        
 
 
-'''def directions(instructions): 
+def directions(instructions): 
     if instructions == "Forward":
         move.forward()
     elif instructions[0] == "Left":
@@ -27,4 +18,4 @@ def handler(instructions):
     elif instructions[0] == "Standing":
         move.stand()
     elif instructions[0] == "Auto":
-        move.autonom()'''
+        move.autonom()
