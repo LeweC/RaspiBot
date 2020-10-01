5c8505ce8febdc1df89165c4fa161526ae88a4a2
LeweC <lewe22lewe@googlemail.com>
2020-10-01 21:53:06 +0200

    Improving sensor movment :wrench:

--- Server/main.py
+++ Server/main.py
@@ -4,16 +4,18 @@ import time
 import random
 
 def handler(instructions):
+    directions(instructions)
+
+
     if instructions[1] == "empty":
         instructions[1] = "0"
     
-    directions(instructions)
-    
     angle = instructions[1]
     intAngle = int(angle)
-    intAngle = intAngle * 5
+    #intAngle = intAngle * 5
     moveUSS(intAngle)
-       
+    #if intAngle
+
 def moveUSS(instructions):
     move.sensor_free(instructions)
 
